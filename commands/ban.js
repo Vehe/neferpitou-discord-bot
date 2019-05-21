1b87ac81ae8450f25a712f13ddad8bca1d3de3d3
v3he <vehedev@gmail.com>
2019-05-21 22:37:41 +0200

    ban funcionality enabled
    Signed-off-by: v3he <vehedev@gmail.com>

---
+++ commands/ban.js
@@ -0,0 +1,15 @@
+module.exports = {
+	name: 'ban',
+	description: 'Banea a un usuario en concreto!',
+	execute(message, args) {
+
+		const member = message.mentions.members.first();
+		if (!member) return message.reply('You need to mention the member you want to ban him');
+
+		return member
+			.ban()
+			.then(() => message.reply(`${member.user.tag} Ha sido baneado. :smiling_imp:`))
+            .catch(error => message.reply('Ops! Algo ha salido mal hoho.'));
+            
+	},
+};
