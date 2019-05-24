b42c5e0aa4bd8745e8130172de2e1d0e3ea2cf14
Vehe <vehedev@gmail.com>
2019-05-24 19:23:29 +0000

    now the bot can whisper onichan to your ear
    Signed-off-by: Vehe <vehedev@gmail.com>

---
+++ commands/onichan.js
@@ -0,0 +1,24 @@
+const ytdl = require('ytdl-core');
+
+module.exports = {
+	name: 'onichan',
+	usage: '!onichan',
+	description: 'Neferpitou te susurra onichan al oido!',
+	execute(message) {
+
+		const serverQueue = message.client.queue.get(message.guild.id);
+		if(!message.member.voiceChannel) return message.reply('Debes estar primero en un canal de voz!');
+		if(serverQueue) serverQueue.voiceChannel.leave();
+
+		let voiceChannel = message.member.voiceChannel;
+
+		voiceChannel.join().then(connection => {
+			connection.playStream(ytdl('https://www.youtube.com/watch?v=tJf9cSSRDrA', { filter: 'audioonly'}))
+                        	.on('end', () => voiceChannel.leave())
+                        	.on('error', (error) => {
+                                	voiceChannel.leave();
+                                	console.error(error);
+                        	});
+		});
+	},
+};
